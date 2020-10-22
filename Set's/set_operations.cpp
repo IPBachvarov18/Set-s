99b75a43053241426ed0f1e3103acd33bcc80479
Emil Ribarev <eiribarev18@codingburgas.bg>
2020-10-22 20:30:36 +0300

    Implement function for sanitising sets

--- Set's/set_operations.cpp
+++ Set's/set_operations.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <vector>
 #include "set_operations.h"
 
@@ -16,4 +17,21 @@ vector <int> setDifference(vector <int> setA, vector <int> setB)
 vector <int> setIntersection(vector <int> setA, vector <int> setB)
 {
 
+}
+
+// Sorts and removes duplicate elements
+vector <int> sanitiseSet(vector <int> setToSanitise)
+{
+	vector <int> sanitisedSet;
+
+	sort(setToSanitise.begin(), setToSanitise.end());
+
+	sanitisedSet.push_back(setToSanitise[0]);
+	for (size_t i = 1; i < setToSanitise.size(); i++) {
+		if (setToSanitise[i] != setToSanitise[i - 1]) {
+			sanitisedSet.push_back(setToSanitise[i]);
+		}
+	}
+
+	return sanitisedSet;
 }
--- Set's/set_operations.h
+++ Set's/set_operations.h
@@ -4,3 +4,5 @@
 std::vector <int> setUnion(std::vector <int> setA, std::vector <int> setB);
 std::vector <int> setDifference(std::vector <int> setA, std::vector <int> setB);
 std::vector <int> setIntersection(std::vector <int> setA, std::vector <int> setB);
+
+std::vector <int> sanitiseSet(std::vector <int> setToSanitise);
