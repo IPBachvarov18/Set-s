305953bcd0f4d30b8953dffe79096724995c405b
pmdyakov18 <59693549+pmdyakov18@users.noreply.github.com>
2020-10-31 21:22:47 +0200

    Fix mainMenu and create checkSet

--- Set's/Set's.cpp
+++ Set's/Set's.cpp
@@ -21,6 +21,18 @@ void enterSet(vector <int> &a)
 	a = sanitiseSet(a);
 }
 
+bool checkSet(vector <int> a)
+{
+    if (a.size() == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
 void printSet(vector <int> a)
 {
     if (a.size() == 0) {
@@ -35,42 +47,70 @@ void printSet(vector <int> a)
 	cout << endl;
 }
 
-bool menu(vector <int> setA, vector<int> setB)
+bool menu(vector <int> setA, vector<int> setB, vector<int> &a)
 {
 
     //*****************************************************************************
     // Switch menu to display the menu.
     //*****************************************************************************
-
     cout << "Please select one of the following options:  " << endl;
-
-    cout << "1: setUnion" << endl;
-    cout << "2: setDifference" << endl;
-    cout << "3: setIntersection" << endl;
-    cout << "4: sanitiseSet" << endl;
+    cout << endl;
+    cout << "1: Enter Set" << endl;
+    cout << "2: setUnion" << endl;
+    cout << "3: setDifference" << endl;
+    cout << "4: setIntersection" << endl;
+    cout << "5: sanitiseSet" << endl;
     cout << "9: Quit" << endl;
-
+    cout << endl;
     int choice;
 
     cout << "Enter your choice: ";
 
     cin >> choice;
-
+    cout << endl;
     switch (choice)
     {
     case 1:
-        cout << "You have chosen setUnion" << endl;
-        setUnion(setA, setB);
+        
+        enterSet(a);
         return true;
     case 2:
-        cout << "You have chosen setDifference" << endl;
-        setDifference(setA, setB);
+        if (checkSet(a) == false)
+        {
+            cout << "You have chosen setUnion" << endl;
+            setUnion(setA, setB);
+            return true;
+        }
+        else
+        {
+            cout << "You need to enter a Set!" << endl;
+        }
         return true;
     case 3:
-        cout << "You have chosen setIntersection" << endl;
-        setIntersection(setA, setB);
+        if (checkSet(a) == false)
+        {
+            cout << "You have chosen setDifference" << endl;
+            setDifference(setA, setB);
+            return true;
+        }
+        else
+        {
+            cout << "You need to enter a Set!" << endl;
+        }
         return true;
     case 4:
+        if (checkSet(a) == false)
+        {
+            cout << "You have chosen setIntersection" << endl;
+            setIntersection(setA, setB);
+            return true;
+        }
+        else
+        {
+            cout << "You need to enter a Set!" << endl;
+        }
+        return true;
+    case 5:
         return true;
     case 9:
         cout << "Goodbye!" << endl;;
@@ -85,9 +125,10 @@ bool menu(vector <int> setA, vector<int> setB)
 int main()
 {
     vector<int> setA = { 1 }, setB = { 1 };
+    vector <int> a = { 1 };
     bool showMenu = true;
     do 
     {
-        showMenu = menu(setA,setB);
+        showMenu = menu(setA,setB,a);
     } while (showMenu);
 }
